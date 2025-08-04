e3962450f3e8d34a91a569a7a304461c98373ac4
Muthiah <122891556+MuthiahAinun@users.noreply.github.com>
2025-08-04 14:32:08 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+from utils.extract import extract_data
+from utils.transform import transform_data
+from utils.load import load_to_csv, load_to_google_sheets, load_to_postgresql
+
+def main():
+    df = extract_data()
+    df_clean = transform_data(df)
+    load_to_csv(df_clean)
+    load_to_google_sheets(df_clean,
+                          credentials_path="fiery-topic-433913-q3-3b7fa3a8e0e9.json",
+                          sheet_url="https://docs.google.com/spreadsheets/d/1JZbYxtif9lSF_SdWA2hV_kjGIdR-CBVM2DMzlkSBTWU/edit?usp=sharing")
+    load_to_postgresql(df_clean)
+
+if __name__ == "__main__":
+    main()
+
