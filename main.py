5f27704ef46582dc3024cdd101bcb56d6e8cbad3
Muthiah <122891556+MuthiahAinun@users.noreply.github.com>
2025-08-04 17:03:37 +0800

    Update main.py

--- main.py
+++ main.py
@@ -8,9 +8,10 @@ def main():
     load_to_csv(df_clean)
     load_to_google_sheets(df_clean,
                           credentials_path="fiery-topic-433913-q3-3b7fa3a8e0e9.json",
-                          sheet_url="https://docs.google.com/spreadsheets/d/1JZbYxtif9lSF_SdWA2hV_kjGIdR-CBVM2DMzlkSBTWU/edit?usp=sharing")
+                          sheet_url="https://docs.google.com/spreadsheets/d/1f83u8QaFKaJPBxP0p-oKlYMXA67BhRaS_wKGuJdNaW4/edit?usp=sharing")
     load_to_postgresql(df_clean)
 
 if __name__ == "__main__":
     main()
+
 
